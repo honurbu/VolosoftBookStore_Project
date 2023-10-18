ac343491d8b159f5c8d9666a22f5ab83885e00da
honurbu <h.onur4377@gmail.com>
2023-10-18 20:52:24 +0300

    Intern Project Ready

---
+++ Volosoft/src/Acme.BookStore.EntityFrameworkCore/Authors/EfCoreAuthorRepository.cs
@@ -0,0 +1,48 @@
+﻿using Acme.BookStore.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+using System.Linq.Dynamic.Core;
+
+namespace Acme.BookStore.Authors
+{
+    public class EfCoreAuthorRepository
+     : EfCoreRepository<BookStoreDbContext, Author, Guid>,
+         IAuthorRepository
+    {
+        public EfCoreAuthorRepository(
+            IDbContextProvider<BookStoreDbContext> dbContextProvider)
+            : base(dbContextProvider)
+        {
+        }
+
+        public async Task<Author> FindByNameAsync(string name)
+        {
+            var dbSet = await GetDbSetAsync();
+            return await dbSet.FirstOrDefaultAsync(author => author.Name == name);
+        }
+
+        public async Task<List<Author>> GetListAsync(
+            int skipCount,
+            int maxResultCount,
+            string sorting,
+            string filter = null)
+        {
+            var dbSet = await GetDbSetAsync();
+            return await dbSet
+                .WhereIf(
+                    !filter.IsNullOrWhiteSpace(),
+                    author => author.Name.Contains(filter)
+                    )
+                .OrderBy(sorting)
+                .Skip(skipCount)
+                .Take(maxResultCount)
+                .ToListAsync();
+        }
+    }
+}
---
+++ Volosoft/src/Acme.BookStore.Web/Pages/Authors/EditModal.cshtml.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using System;
+using Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Form;
+using Acme.BookStore.Authors;
+
+namespace Acme.BookStore.Web.Pages.Authors
+{
+    public class EditModalModel : BookStorePageModel
+    {
+        [BindProperty]
+        public EditAuthorViewModel Author { get; set; }
+
+        private readonly IAuthorAppService _authorAppService;
+
+        public EditModalModel(IAuthorAppService authorAppService)
+        {
+            _authorAppService = authorAppService;
+        }
+
+        public async Task OnGetAsync(Guid id)
+        {
+            var authorDto = await _authorAppService.GetAsync(id);
+            Author = ObjectMapper.Map<AuthorDto, EditAuthorViewModel>(authorDto);
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await _authorAppService.UpdateAsync(
+                Author.Id,
+                ObjectMapper.Map<EditAuthorViewModel, UpdateAuthorDto>(Author)
+            );
+
+            return NoContent();
+        }
+
+        public class EditAuthorViewModel
+        {
+            [HiddenInput]
+            public Guid Id { get; set; }
+
+            [Required]
+            [StringLength(AuthorConsts.MaxNameLength)]
+            public string Name { get; set; }
+
+            [Required]
+            [DataType(DataType.Date)]
+            public DateTime BirthDate { get; set; }
+
+            [TextArea]
+            public string ShortBio { get; set; }
+        }
+    }
+}
